ad5b18cc499c65401f1ad785f4e206356cd54543
Gabriel Dal Borgo <gabriel.dalborgo@gmail.com>
2025-08-18 16:34:20 -0300

    Error state added

--- src/features/transactions/transactions-record.tsx
+++ src/features/transactions/transactions-record.tsx
@@ -1,41 +1,19 @@
-import { List, ListSkeleton } from "./components/list";
 import { useTransactions } from "./hooks/use-transactions";
-import filterIcon from "../../assets/filter-icon.svg";
-import exportIcon from "../../assets/export-icon.svg";
 import { EmptyState } from "./components/empty-state";
+import { ErrorState } from "./components/error-state";
+import { Header } from "./components/header";
+import { List, ListSkeleton } from "./components/list";
 
 export function TransactionsRecord() {
   const { transactions, state } = useTransactions()
 
   return (
     <div className="pr-2 pl-2">
-      <div className="flex items-center justify-between h-12 pb-2">
-        <div 
-          className="text-[#313643]"
-          style={{
-            fontFamily: 'Public Sans',
-            fontWeight: 600,
-            fontStyle: 'normal',
-            fontSize: '14px',
-            lineHeight: '100%',
-            letterSpacing: '0%',
-            verticalAlign: 'middle'
-          }}
-        >
-          Historial de transacciones
-        </div>
-        <div className="flex items-center gap-6">
-          <button className="w-6 h-6 flex items-center justify-center">
-            <img src={filterIcon} alt="Filter" className="w-6 h-6" />
-          </button>
-          <button className="w-6 h-6 flex items-center justify-center">
-            <img src={exportIcon} alt="Export" className="w-6 h-6" />
-          </button>
-        </div>
-      </div>
+      <Header />
       {state === "loading" && <ListSkeleton count={10} />}
-      {state === "success" && <List items={transactions} />}
+      {state === "error" && <ErrorState />}
       {state === "empty" && <EmptyState />}
+      {state === "success" && <List items={transactions} />}
     </div>
   )
 }
