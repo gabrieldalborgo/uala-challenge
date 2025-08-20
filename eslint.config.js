cf2afcd22560efb233ba66ef5fd3d60e6b31ed03
Gabriel Dal Borgo <gabriel.dalborgo@gmail.com>
2025-08-20 15:21:52 -0300

    Improve eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist', 'tests']),
+  globalIgnores(['dist', 'tests', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
